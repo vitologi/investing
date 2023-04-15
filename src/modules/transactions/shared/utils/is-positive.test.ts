14757e060b2282bb48a9ed92b59adfd3968d3ae1
Spektr <spektrasm@gmail.com>
2023-04-15 22:52:38 +0400

    add test to is-positive util

---
+++ src/modules/transactions/shared/utils/is-positive.test.ts
@@ -0,0 +1,14 @@
+import {isPositive} from "./is-positive";
+
+describe('isPositive', () => {
+
+  test('return true if number is positive or zero', () => {
+    expect(isPositive(0)).toBe(true);
+    expect(isPositive(1245)).toBe(true);
+  });
+
+  test('return false if number is negative', () => {
+    expect(isPositive(-1)).toBe(false);
+  });
+
+});
