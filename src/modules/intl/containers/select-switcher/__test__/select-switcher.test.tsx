6e1257971fb2b07d5d5ab51ab96c398e42fe0894
Spektr <spektrasm@gmail.com>
2023-05-21 15:37:24 +0400

    cover select-switcher.test.tsx

--- src/modules/intl/containers/select-switcher/__test__/select-switcher.test.tsx
+++ src/modules/intl/containers/select-switcher/__test__/select-switcher.test.tsx
@@ -2,15 +2,26 @@ import {render} from "../../../../../test-utils";
 import {SelectSwitcher} from "../select-switcher";
 import userEvent from "@testing-library/user-event";
 import {LanguageCode} from "../../../shared/enums/language-code";
+import * as intlSelector from "../../../store/intl.selector";
+import {IntlStore} from "../../../store/intl.store";
 
-jest.mock("")
+jest.mock("../../../store/intl.selector", ()=>({
+  __esModule: true,
+  useIntlStore: jest.fn(),
+}))
 
 describe('SelectSwitcher', ()=>{
-  // let store: jest.Mocked<Intl>
-  //
-  // beforeEach(()=>{
-  //
-  // })
+  let store: jest.Mocked<IntlStore>;
+
+  beforeEach(()=>{
+    store = {
+      locale: LanguageCode.En,
+      setLanguage: jest.fn(),
+    } as unknown as jest.Mocked<IntlStore>;
+
+    (intlSelector as jest.Mocked<typeof import("../../../store/intl.selector")>)
+      .useIntlStore.mockReturnValue(store);
+  })
 
   test('should be rendered', async ()=>{
     const {getByRole} = render(<SelectSwitcher />);
@@ -23,10 +34,12 @@ describe('SelectSwitcher', ()=>{
     expect(getAllByRole('option').length).toBe(Object.values(LanguageCode).length);
   });
 
-  // test('should switch language', async ()=>{
-  //   const {getByRole,getAllByRole} = render(<SelectSwitcher />);
-  //   await userEvent.click(getByRole('button'));
-  //   const ruButton = getByRole('option', {name: /ru/i});
-  //   await userEvent.click(ruButton);
-  // });
+  test('should switch language', async ()=>{
+    const {getByRole} = render(<SelectSwitcher />);
+    await userEvent.click(getByRole('button'));
+    const ruButton = getByRole('option', {name: /ru/i});
+    expect(store.setLanguage).toHaveBeenCalledTimes(0);
+    await userEvent.click(ruButton);
+    expect(store.setLanguage).toHaveBeenCalledTimes(1);
+  });
 })
