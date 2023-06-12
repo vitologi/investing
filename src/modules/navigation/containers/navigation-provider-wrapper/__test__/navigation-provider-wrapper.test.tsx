289920f930f46f6acd0f501c38b80ff3e03ffe4c
Spektr <spektrasm@gmail.com>
2023-06-12 23:58:03 +0400

    try to cover nav provider wrapper

---
+++ src/modules/navigation/containers/navigation-provider-wrapper/__test__/navigation-provider-wrapper.test.tsx
@@ -0,0 +1,36 @@
+// import enLocale from '../../../../intl/locale/en.json';
+import {NavigationProviderWrapper} from "../navigation-provider-wrapper";
+import {render} from "../../../../../test-utils";
+import {Container} from "inversify";
+import {buildIoc} from "../../../../../store/build-ioc";
+import {IntlStore} from "../../../../intl/store/intl.store";
+import {NavigationPanelStore} from "../../../store/navigation-panel.store";
+import {DrawersPanelStore} from "../../../store/drawers-panel.store";
+import {DiProvider} from "../../../../../shared/components/di/di.provider";
+
+
+describe('NavigationProviderWrapper', () => {
+  let di: Container;
+
+  beforeAll(() => {
+    di = buildIoc([IntlStore, NavigationPanelStore, DrawersPanelStore]);
+  })
+
+  test('renders children and sets the document title', () => {
+    const children = <div>Test Children</div>;
+    const store = di.get<NavigationPanelStore>(NavigationPanelStore.name);
+    store.title = "app.titles.main";
+
+    const {getByText} = render(
+      <DiProvider container={di}>
+        <NavigationProviderWrapper>{children}</NavigationProviderWrapper>
+      </DiProvider>
+    );
+
+    // Verify that the children are rendered
+    expect(getByText('Test Children')).toBeInTheDocument();
+
+    // Verify that the document title is set correctly
+    // expect(document.title).toEqual(title); // Replace 'Expected Title' with the expected value
+  });
+});
